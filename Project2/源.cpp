01a614bcbb4bab39b94431f0bf8b9d2e661bc08a
翔鹏 丁 <1071871029@qq.com>
2018-08-31 10:42:38 +0800

    添加项目文件。

---
+++ Project2/源.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+// use and displays a variable
+int main()
+{
+	using namespace std;
+	int carrots;
+	carrots = 25;
+	cout << "I have ";
+	cout << carrots;
+	cout << " carrots.";
+	cout << endl;
+	carrots = carrots - 1;
+	cout << "Crunch, crunch. Now I have " << carrots << " carrots." << endl;
+	return 0; 
+}
